2fab38920523f11ccadeb8b2c5a2b4ee92808b2c
tsantos <tales.augusto.santos@gmail.com>
2021-12-26 16:18:22 -0300

    Fix assertion

--- core/src/test/kotlin/com/messengerk/core/Assertions.kt
+++ core/src/test/kotlin/com/messengerk/core/Assertions.kt
@@ -4,6 +4,9 @@ import strikt.api.Assertion
 
 inline fun <reified T> Assertion.Builder<Envelope<Any>>.contains(): Assertion.Builder<Envelope<Any>> {
     return assert("Envelope contains stamp of type" + T::class.toString()) {
-        it.contains<T>()
+        when(it.contains<T>()) {
+            true -> pass()
+            else -> fail()
+        }
     }
 }
