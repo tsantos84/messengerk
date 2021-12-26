d7e1e1edcb6fdb4e643a16233031b034d87d8961
tsantos <tales.augusto.santos@gmail.com>
2021-12-26 16:19:01 -0300

    Added "reflect" to implementation dependency

--- spring-boot-starter/build.gradle.kts
+++ spring-boot-starter/build.gradle.kts
@@ -1,6 +1,7 @@
 dependencies {
     api(project(":core"))
     implementation ("org.springframework.boot:spring-boot-starter:2.6.2")
+    implementation(kotlin("reflect"))
     testImplementation ("org.springframework.boot:spring-boot-starter-test:2.6.2") {
         exclude(group = "org.junit.vintage", module = "junit-vintage-engine")
     }
